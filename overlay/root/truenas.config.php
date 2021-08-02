5ea5db2530122bff24eb9b1f2a92ba27e3cd0c19
Louis Chemineau <louis@chmn.me>
2021-08-02 16:33:34 +0200

    Add user limit
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- overlay/root/truenas.config.php
+++ overlay/root/truenas.config.php
@@ -1,5 +1,7 @@
 <?php
 $CONFIG = array(
+  'one-click-instance' => true,
+  'one-click-instance.user-limit' => 100,
   'memcache.distributed' => '\OC\Memcache\Redis',
   'memcache.locking' => '\OC\Memcache\Redis',
   'redis' => array(
