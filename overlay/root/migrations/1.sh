cdb6a72476f5fa0d855d87053d2518e3aa628ae1
Tom Olivier <contact.tom60@proton.me>
2023-10-26 16:01:54 +0200

    Migration from Oracle MySQL to MariaDB

--- overlay/root/migrations/1.sh
+++ overlay/root/migrations/1.sh
@@ -10,7 +10,7 @@ sysrc -f /etc/rc.conf redis_enable="YES"
 sysrc -f /etc/rc.conf fail2ban_enable="YES"
 service redis start 2>/dev/null
 service fail2ban start 2>/dev/null
-service mysql-server start 2>/dev/null
+service mysql-server start > &2 /dev/null
 
 # Wait for mysql to be up
 until mysql --user dbadmin --password="$(cat /root/dbpassword)" --execute "SHOW DATABASES" > /dev/null
