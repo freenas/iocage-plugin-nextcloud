40d9cb34c931b40b98bdfe4b3ab654004a052382
Tom Olivier <contact.tom60@proton.me>
2024-02-11 11:53:04 +0100

    Fix migration and add mjs, wasm mime type

--- overlay/root/migrations/1.sh
+++ overlay/root/migrations/1.sh
@@ -10,7 +10,7 @@ sysrc -f /etc/rc.conf redis_enable="YES"
 sysrc -f /etc/rc.conf fail2ban_enable="YES"
 service redis start 2>/dev/null
 service fail2ban start 2>/dev/null
-service mysql-server start > &2 /dev/null
+service mysql-server start 2> /dev/null
 
 # Wait for mysql to be up
 until mysql --user dbadmin --password="$(cat /root/dbpassword)" --execute "SHOW DATABASES" > /dev/null
@@ -22,10 +22,5 @@ done
 # Change cron execution method
 su -m www -c "php /usr/local/www/nextcloud/occ background:cron"
 
-# Install default applications
-su -m www -c "php /usr/local/www/nextcloud/occ app:install contacts"
-su -m www -c "php /usr/local/www/nextcloud/occ app:install calendar"
-su -m www -c "php /usr/local/www/nextcloud/occ app:install notes"
-su -m www -c "php /usr/local/www/nextcloud/occ app:install deck"
-su -m www -c "php /usr/local/www/nextcloud/occ app:install spreed"
-su -m www -c "php /usr/local/www/nextcloud/occ app:install mail"
+# Upgrade Nextcloud
+su -m www -c "php /usr/local/www/nextcloud/occ upgrade"
